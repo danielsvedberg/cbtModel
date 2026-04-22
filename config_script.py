e0ad88b003661f81a691b257ec9ef50febbe6dac
Daniel Svedberg <dsvedberg@brandeis.edu>
2026-04-22 09:09:37 -0400

    further decrease of tau still yields good gradient

--- config_script.py
+++ config_script.py
@@ -72,8 +72,8 @@ default_config = dict(
     U=1,      # input dim
     O=1,      # output dimension
     # Model Hyperparameters
-    tau_x=10,
-    tau_z=20,
+    tau_x=5,
+    tau_z=10,
     noise_std=0.05,  # Standard deviation of noise
     # Timing (task) parameters
     dt=10, # ms
